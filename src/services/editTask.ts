618a7eb714bc8943ef3e83dd03705d37db768790
Allan <allan.mendes0976@gmail.com>
2024-10-28 22:39:46 -0300

    exercises

--- src/services/editTask.ts
+++ src/services/editTask.ts
@@ -5,7 +5,7 @@ export class EditTask implements TaskProtocol{
   constructor(private target: HTMLElement) {}
 
   execute() {
-    const msg = window.prompt('Alterar a Tarefa: ') as string;
+    const msg = window.prompt('Alterar Tarefa: ') as string;
     if(msg === '' || !msg) return this.target.classList.remove('paragrafoVisivel');
     this.target.classList.remove('paragrafoVisivel');
     this.target.innerHTML = msg;
